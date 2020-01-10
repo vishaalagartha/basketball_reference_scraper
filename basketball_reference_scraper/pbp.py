b52ad62d4b7c23287fd4b51e151e6aa1d8eec33f
vishaalagartha <vishaalagartha@gmail.com>
2020-01-10 13:50:57 -0800

    pbp fixes

--- basketball_reference_scraper/pbp.py
+++ basketball_reference_scraper/pbp.py
@@ -2,71 +2,54 @@ import pandas as pd
 from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
-from pyppeteer import launch
-import asyncio
 
 try:
     from utils import get_game_suffix
 except:
     from basketball_reference_scraper.utils import get_game_suffix
 
-async def get_pbp_helper(suffix):
+def get_pbp_helper(suffix):
     selector = f'#pbp'
-    browser = await launch()
-    page = await browser.newPage()
-    await page.goto(f'https://www.basketball-reference.com/boxscores/pbp{suffix}')
-    await page.waitForSelector(f'{selector}')
-    table = await page.querySelectorEval(f'{selector}', '(element) => element.outerHTML')
-    await browser.close()
-    return pd.read_html(table)[0]
+    r = get(f'https://www.basketball-reference.com/boxscores/pbp{suffix}')
+    if r.status_code==200:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        table = soup.find('table', attrs={'id': 'pbp'})
+        return pd.read_html(str(table))[0]
 
 def format_df(df1):
-    i0 = '1st Q'
-    i1 = '1st Q.1'
-    i2 = '1st Q.3'
-    i3 = '1st Q.5'
-    t1 = df1.iloc[0][i1].upper()
-    t2 = df1.iloc[0][i3].upper()
+    df1.columns = list(map(lambda x: x[1], list(df1.columns)))
+    t1 = list(df1.columns)[1].upper()
+    t2 = list(df1.columns)[5].upper()
     q = 1
-    df = pd.DataFrame(columns=['QUARTER', 'TIME_REMAINING', f'{t1}_ACTION', f'{t2}_ACTION', f'{t1}_SCORE', f'{t2}_SCORE'])
+    df = None
     for index, row in df1.iterrows():
         d = {'QUARTER': float('nan'), 'TIME_REMAINING': float('nan'), f'{t1}_ACTION': float('nan'), f'{t2}_ACTION': float('nan'), f'{t1}_SCORE': float('nan'), f'{t2}_SCORE': float('nan')}
-        if row[i0]=='2nd Q':
+        if row['Time']=='2nd Q':
             q = 2
-        elif row[i0]=='3rd Q':
+        elif row['Time']=='3rd Q':
             q = 3
-        elif row[i0]=='4th Q':
+        elif row['Time']=='4th Q':
             q = 4
-        try:
-            t = pd.to_datetime(row[i0])
-            if t.hour==12:
-                if q == 'Q1':
-                    d['QUARTER'] = q
-                    d['TIME_REMAINING'] = row[i0]
-                    d[f'{t1}_ACTION'] = row[i1]
-                    d[f'{t2}_ACTION'] = row[i1]
-                    d[f'{t1}_SCORE'] = 0
-                    d[f'{t2}_SCORE'] = 0
-                    df = df.append(d, ignore_index=True)
-                continue
-        except:
-            continue
+        elif 'OT' in row['Time']:
+            q = row['Time'][0]+'OT'
         try:
             d['QUARTER'] = q
-            d['TIME_REMAINING'] = row[i0]
-            d[f'{t1}_ACTION'] = row[i1]
-            d[f'{t2}_ACTION'] = row[i3]
-            scores = row[i2].split('-')
+            d['TIME_REMAINING'] = row['Time']
+            scores = row['Score'].split('-')
             d[f'{t1}_SCORE'] = int(scores[0])
             d[f'{t2}_SCORE'] = int(scores[1])
+            d[f'{t1}_ACTION'] = row[list(df1.columns)[1]]
+            d[f'{t2}_ACTION'] = row[list(df1.columns)[5]]
+            if df is None:
+                df = pd.DataFrame(columns = list(d.keys()))
             df = df.append(d, ignore_index=True)
         except:
-            pass
+            continue
     return df
 
 def get_pbp(date, team1, team2):
     date = pd.to_datetime(date)
     suffix = get_game_suffix(date, team1, team2).replace('/boxscores', '')
-    df = asyncio.get_event_loop().run_until_complete(get_pbp_helper(suffix))
+    df = get_pbp_helper(suffix)
     df = format_df(df)
     return df
