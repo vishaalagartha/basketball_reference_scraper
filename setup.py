14a7a0ac49765cca25df0b4620886e8c412467ef
vishaalagartha <vishaalagartha@gmail.com>
2020-09-21 09:37:38 -0700

    fixes #23

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="basketball_reference_scraper",
-    version="1.0.17",
+    version="1.0.18",
     author="Vishaal Agartha",
     author_email="vishaalagartha@gmail.com",
     license="MIT",
