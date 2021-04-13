daf2f0d24b695cba9bec3d6c321fc66b0f438218
bellmatthewf <41313531+bellmatthewf@users.noreply.github.com>
2021-04-12 21:13:55 -0400

    Fix not sorting matches when ask_matches is false

--- basketball_reference_scraper/lookup.py
+++ basketball_reference_scraper/lookup.py
@@ -52,6 +52,8 @@ def lookup(player, ask_matches = True):
     if len(matches) == 1 or ask_matches == False:
         print("You searched for \"{}\"\n{} result found.\n{}".format(player, len(matches), matches[0][0]))
         print("Results for {}:\n".format(matches[0][0]))
+        if len(matches) > 1:
+            matches.sort(key=lambda tup: tup[1])
         return matches[0][0]
 
     elif len(matches) > 1:
