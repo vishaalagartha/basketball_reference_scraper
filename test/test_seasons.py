82c4e2a82dcdd435af84a82c606c3f13cf025ebc
JD Rudie <rudiejd@miamioh.edu>
2021-07-25 17:21:16 -0400

    fix issue with empty tables on some season months

--- test/test_seasons.py
+++ test/test_seasons.py
@@ -8,6 +8,13 @@ class TestSeason(unittest.TestCase):
                 'HOME_PTS']
         self.assertListEqual(list(df.columns), expected_columns)
 
+    def test_get_schedule_weird_season(self):
+        for season in (1971, 1953):
+            cur_season = get_schedule(season)
+            expected_columns = ['DATE', 'VISITOR', 'VISITOR_PTS', 'HOME',
+                    'HOME_PTS']
+            self.assertListEqual(list(cur_season.columns), expected_columns)
+
     def test_get_standings(self):
         d = get_standings()
         self.assertListEqual(list(d.keys()), ['EASTERN_CONF', 'WESTERN_CONF'])
@@ -15,6 +22,15 @@ class TestSeason(unittest.TestCase):
         df = d['WESTERN_CONF']
         expected_columns = ['TEAM', 'W', 'L', 'W/L%', 'GB', 'PW', 'PL', 'PS/G', 'PA/G']
         self.assertListEqual(list(df.columns), expected_columns)
+
+    def test_get_standings_weird_season(self):
+        for season in (1971, 1953):
+            d = get_standings(season)
+            self.assertListEqual(list(d.keys()), ['EASTERN_CONF', 'WESTERN_CONF'])
+
+            df = d['WESTERN_CONF']
+            expected_columns = ['TEAM', 'W', 'L', 'W/L%', 'GB', 'PW', 'PL', 'PS/G', 'PA/G']
+            self.assertListEqual(list(df.columns), expected_columns)
 
 if __name__ == '__main__':
     unittest.main()
