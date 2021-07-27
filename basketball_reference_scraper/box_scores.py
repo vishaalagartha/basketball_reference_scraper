c7ff189e97a180a2967d42633060ab77393e1091
JD Rudie <rudiejd@miamioh.edu>
2021-07-26 21:56:34 -0400

    add getting all star game box scores

--- basketball_reference_scraper/box_scores.py
+++ basketball_reference_scraper/box_scores.py
@@ -2,11 +2,14 @@ import pandas as pd
 from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
+import re
 
 try:
     from utils import get_game_suffix, remove_accents
+    from players import get_stats 
 except:
     from basketball_reference_scraper.utils import get_game_suffix, remove_accents
+    from basketball_reference_scraper.players import get_stats
 
 def get_box_scores(date, team1, team2, period='GAME', stat_type='BASIC'):
     date = pd.to_datetime(date)
@@ -31,3 +34,57 @@ def get_box_scores(date, team1, team2, period='GAME', stat_type='BASIC'):
         reserve_index = df2[df2['PLAYER']=='Reserves'].index[0]
         df2 = df2.drop(reserve_index).reset_index().drop('index', axis=1)
         return {team1: df1, team2: df2}
+
+def get_all_star_box_score(year: int):
+    """ Returns box star for all star game in a given year
+
+    Args:
+        year (int): the year of the all star game 
+
+    Raises:
+        ValueError: if invalid year is intered 
+        ConnectionError: when server cannot be reached
+
+    Returns:
+        dict: dictionary containing entries (team name, box score dataframe) for each team 
+    """
+    if year >= datetime.now().year or year < 1951:
+        raise ValueError('Please enter a valid year')
+    r = get(f'https://www.basketball-reference.com/allstar/NBA_{year}.html')
+    if r.status_code == 200:
+        dfs = []
+        soup = BeautifulSoup(r.content, 'html.parser')
+        team_names = list(map(lambda el: el.text, soup.select('div.section_heading > h2')[1:3]))
+        for table in soup.find_all('table')[1:3]:
+            df = pd.read_html(str(table))[0]
+            df.columns = list(map(lambda x: x[1], list(df.columns)))
+            df.rename(columns = {'Starters': 'PLAYER', 'Tm': 'TEAM'}, inplace=True)
+            reserve_index = df[df['PLAYER']=='Reserves'].index[0]
+            df = df.drop(reserve_index).reset_index().drop('index', axis=1) 
+            #drop team totals row (always last), totals row
+            totals_index = df[df['MP'] == 'Totals'].index[0]
+            df = df.drop(totals_index).reset_index().drop('index', axis=1) 
+            df = df.drop(df.tail(1).index).reset_index().drop('index', axis=1) 
+            # sometimes there is a nan player
+            dfs.append(df)
+        res = {team_names[0]: dfs[0], team_names[1]: dfs[1]}
+        # add DNPs for all-star roster purposes
+        # the all-star team each dnp is on is in parens. for some reasons this captures parens
+        dnp_allstar_teams = re.findall(r'\((.*?)\)', soup.select('ul.page_index > li > div')[0].text)
+        for i, dnp in enumerate(map(lambda el: el.text, soup.select('ul.page_index > li > div > a'))):
+            # check if the player is already in the dataframe because sometimes replacements are
+            # listed twice (e.g. 1980)
+            as_team = dnp_allstar_teams[i]
+            if dnp not in res[as_team]['PLAYER'].values:
+                new_row = {col: 0 for col in df.columns if col != 'PLAYER' and col != 'TEAM'}
+                new_row['PLAYER'] = dnp
+                # infer the added player's real team 
+                stats_df = get_stats(dnp, ask_matches=False)
+                team = stats_df.query(f'SEASON == {year-1}-{year}').head(1)['TEAM']
+                new_row['TEAM'] = team
+                # set players allstar team from regexp
+                res[as_team] = res[as_team].append(new_row, ignore_index=True)
+        return res 
+    else:
+        raise ConnectionError('Request to basketball reference failed')
+
--- test/test_box_scores.py
+++ test/test_box_scores.py
@@ -1,5 +1,5 @@
 import unittest
-from basketball_reference_scraper.box_scores import get_box_scores 
+from basketball_reference_scraper.box_scores import get_box_scores, get_all_star_box_score 
 
 class TestBoxScores(unittest.TestCase):
     def test_get_box_scores(self):
@@ -9,6 +9,27 @@ class TestBoxScores(unittest.TestCase):
         df = d['DEN']
         expected_columns = ['PLAYER', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '+/-']
         self.assertListEqual(list(df.columns), expected_columns)
+
+    def test_get_all_star_box_score(self):
+        d = get_all_star_box_score(2020)
+        df = d['Team LeBron']
+        # they dont record +/- in ASG :(
+        expected_columns = ['PLAYER', 'TEAM', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+        for item in df.columns:
+            if item not in expected_columns:
+                print(item) 
+        self.assertSetEqual(set(df.columns), set(expected_columns))
+
+        # check for one of the dnp players
+        self.assertTrue('Damian Lillard' in df['PLAYER'].values)
+        d2 = get_all_star_box_score(1980)
+        df = d2['East']
+        # check uniqueness of all star names
+        expected_players = ['George Gervin', 'Eddie Johnson', 'Moses Malone', 'Julius Erving', 'John Drew', 'Elvin Hayes', 'Dan Roundfield', 'Larry Bird', 'Tiny Archibald', 'Bill Cartwright', 'Micheal Ray Richardson', 'Dave Cowens']
+        for player in df['PLAYER'].values:
+            if player not in df['PLAYER'].values:
+                print(player)
+        self.assertListEqual(expected_players, list(df['PLAYER'].values))
 
 if __name__ == '__main__':
     unittest.main()
