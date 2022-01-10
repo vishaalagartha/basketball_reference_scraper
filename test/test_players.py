3e88ee7ac734ba36be73502facf4929d087ef851
JD Rudie <rudiejd@miamioh.edu>
2022-01-10 17:28:27 -0500

    fix: resolve lookup error for players with suffix number greater than 1

--- test/test_players.py
+++ test/test_players.py
@@ -5,6 +5,8 @@ class TestPlayers(unittest.TestCase):
     def test_get_stats(self):
         expected_columns = ['SEASON', 'AGE', 'TEAM', 'LEAGUE', 'POS', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
         df = get_stats('Joe Johnson')
+        self.assertCountEqual(list(df.columns), expected_columns)
+
         df = get_stats('LaMarcus Aldridge') 
         self.assertCountEqual(list(df.columns), expected_columns)
 
@@ -24,6 +26,7 @@ class TestPlayers(unittest.TestCase):
 
     def test_get_player_headshot(self):
         expected_url = 'https://d2cwpp38twqe55.cloudfront.net/req/202006192/images/players/bryanko01.jpg'
+        
         url = get_player_headshot('Kobe Bryant')
         self.assertEqual(url, expected_url)
 
