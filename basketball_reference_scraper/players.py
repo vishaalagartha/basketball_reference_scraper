57c041aa03a329249f70fabeccc70de3bafa8c3d
Vishaal Agartha <vishaalagartha@gmail.com>
2024-03-12 19:21:00 -0700

    Fixes player stat fetching

--- basketball_reference_scraper/players.py
+++ basketball_reference_scraper/players.py
@@ -5,69 +5,67 @@ from bs4 import BeautifulSoup
 try:
     from utils import get_player_suffix
     from lookup import lookup
-    from request_utils import get_wrapper
+    from request_utils import get_selenium_wrapper
 except:
     from basketball_reference_scraper.utils import get_player_suffix
-    from basketball_reference_scraper.request_utils import get_wrapper
+    from basketball_reference_scraper.request_utils import get_wrapper, get_selenium_wrapper
     from basketball_reference_scraper.lookup import lookup
 
 def get_stats(_name, stat_type='PER_GAME', playoffs=False, career=False, ask_matches = True):
     name = lookup(_name, ask_matches)
     suffix = get_player_suffix(name)
-    if suffix:
-        suffix = suffix.replace('/', '%2F')
-    else:
+    if not suffix:
         return pd.DataFrame()
-    selector = stat_type.lower()
+    stat_type = stat_type.lower()
     if playoffs:
-        selector = 'playoffs_'+selector
-    r = get_wrapper(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url={suffix}&div=div_{selector}')
-    if r.status_code==200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        table = soup.find('table')
-        if table is None:
-            return pd.DataFrame()
-        df = pd.read_html(str(table))[0]
-        df.rename(columns={'Season': 'SEASON', 'Age': 'AGE',
-                  'Tm': 'TEAM', 'Lg': 'LEAGUE', 'Pos': 'POS'}, inplace=True)
-        if 'FG.1' in df.columns:
-            df.rename(columns={'FG.1': 'FG%'}, inplace=True)
-        if 'eFG' in df.columns:
-            df.rename(columns={'eFG': 'eFG%'}, inplace=True)
-        if 'FT.1' in df.columns:
-            df.rename(columns={'FT.1': 'FT%'}, inplace=True)
+        xpath = f"//table[@id='playoffs_{stat_type}']"
+    else:
+        xpath = f"//table[@id='{stat_type}']"
+    table = get_selenium_wrapper(f'https://www.basketball-reference.com/{suffix}', xpath)
+    if table is None:
+        return pd.DataFrame()
+    df = pd.read_html(table)[0]
+    df.rename(columns={'Season': 'SEASON', 'Age': 'AGE',
+                'Tm': 'TEAM', 'Lg': 'LEAGUE', 'Pos': 'POS', 'Awards': 'AWARDS'}, inplace=True)
+    if 'FG.1' in df.columns:
+        df.rename(columns={'FG.1': 'FG%'}, inplace=True)
+    if 'eFG' in df.columns:
+        df.rename(columns={'eFG': 'eFG%'}, inplace=True)
+    if 'FT.1' in df.columns:
+        df.rename(columns={'FT.1': 'FT%'}, inplace=True)
 
-        career_index = df[df['SEASON']=='Career'].index[0]
-        if career:
-            df = df.iloc[career_index+2:, :]
-        else:
-            df = df.iloc[:career_index, :]
+    career_index = df[df['SEASON']=='Career'].index[0]
+    if career:
+        df = df.iloc[career_index+2:, :]
+    else:
+        df = df.iloc[:career_index, :]
 
-        df = df.reset_index().drop('index', axis=1)
-        return df
+    df = df.reset_index().drop('index', axis=1)
+    return df
 
 
 def get_game_logs(_name, year, playoffs=False, ask_matches=True):
     name = lookup(_name, ask_matches)
-    suffix = get_player_suffix(name).replace('/', '%2F').replace('.html', '')
+    suffix = get_player_suffix(name).replace('.html', '')
     if playoffs:
-        selector = 'div_pgl_basic_playoffs'
+        xpath = f"//table[@id='pgl_basic_playoffs']"
+        url = f'https://www.basketball-reference.com/{suffix}/gamelog-playoffs'
     else:
-        selector = 'div_pgl_basic'
-    r = get_wrapper(f'https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url={suffix}%2Fgamelog%2F{year}%2F&div={selector}')
-    if r.status_code==200:
-        soup = BeautifulSoup(r.content, 'html.parser')
-        table = soup.find('table')
-        if table:
-            df = pd.read_html(str(table))[0]
-            df.rename(columns = {'Date': 'DATE', 'Age': 'AGE', 'Tm': 'TEAM', 'Unnamed: 5': 'HOME/AWAY', 'Opp': 'OPPONENT',
-                'Unnamed: 7': 'RESULT', 'GmSc': 'GAME_SCORE'}, inplace=True)
-            df['HOME/AWAY'] = df['HOME/AWAY'].apply(lambda x: 'AWAY' if x=='@' else 'HOME')
-            df = df[df['Rk']!='Rk']
-            df = df.drop(['Rk', 'G'], axis=1)
-            df['DATE'] = pd.to_datetime(df['DATE'])
-            df = df[df['GS'] == '1'].reset_index(drop=True)          
-            return df
+        xpath = f"//table[@id='pgl_basic']"
+        url = f'https://www.basketball-reference.com/{suffix}/gamelog/{year}'
+    table = get_selenium_wrapper(url, xpath)
+    if not table:
+        return pd.DataFrame([])
+    df = pd.read_html(table)[0]
+    df.rename(columns = {'Date': 'DATE', 'Age': 'AGE', 'Tm': 'TEAM', 'Unnamed: 5': 'HOME/AWAY', 'Opp': 'OPPONENT',
+        'Unnamed: 7': 'RESULT', 'GmSc': 'GAME_SCORE', 'Series': 'SERIES' }, inplace=True)
+    df['HOME/AWAY'] = df['HOME/AWAY'].apply(lambda x: 'AWAY' if x=='@' else 'HOME')
+    df = df[df['Rk']!='Rk']
+    df = df.drop(['Rk', 'G'], axis=1).reset_index(drop=True)
+    if not playoffs:
+        df['DATE'] = pd.to_datetime(df['DATE'])
+    #df = df[df['GS'] == '1'].reset_index(drop=True)          
+    return df
 
 def get_player_headshot(_name, ask_matches=True):
     name = lookup(_name, ask_matches)
