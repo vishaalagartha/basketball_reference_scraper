8585ecbeb98c222c8b950ea924bbfb84d285a051
Vishaal Agartha <vishaalagartha@gmail.com>
2024-03-12 19:20:44 -0700

    Adds selenium wrapper

--- basketball_reference_scraper/request_utils.py
+++ basketball_reference_scraper/request_utils.py
@@ -1,5 +1,23 @@
 from requests import get
-from time import sleep
+from time import sleep, time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.add_argument('--headless=new')
+driver = webdriver.Chrome(options=options)
+
+def get_selenium_wrapper(url, xpath, clickable_xpath=None):
+    try:
+        driver.get(url)
+        element = driver.find_element(By.XPATH, xpath)
+        return f'<table>{element.get_attribute("innerHTML")}</table>'
+    except:
+        print('Error obtaining data table.')
+        return None
 
 def get_wrapper(url):
     r = get(url)
